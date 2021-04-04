56ffd6656b4a094333598056c1a63f4c15350051
shreyashtalele <80088633+shreyashtalele@users.noreply.github.com>
2021-04-04 12:28:28 +0530

    Create size of int float double char.cpp

---
+++ size of int float double char.cpp
@@ -0,0 +1,10 @@
+include <iostream>
+using namespace std;
+int main()
+{ 
+cout<<"size of int is "<<sizeof (int)<<"bytes"<<endl;
+cout<<"size of float is"<<sizeof(float)<<"bytes"<<endl;
+cout<<"size of double is"<<sizeof (double)<<"bytes"<<endl;
+cout<<"size of char is"<<sizeof(char)<<"bytes"<<endl;
+return 0;
+}
